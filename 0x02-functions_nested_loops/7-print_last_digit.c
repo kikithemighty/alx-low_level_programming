163568ebed662013cbaf23a616891cfbc5e30555
kikithemighty <shekinahglorymoshepeter02@gmail.com>
2023-05-26 15:44:11 -0700

    7-main.c 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @r: function that prints the last digit of a number
+ *
+ * Return: return -1 if r is negative and 1 if r is positive
+ */
+
+int print_last_digit(int r)
+{
+	int l;
+
+	l = r % 10;
+	if (l < 0)
+	{
+		_putchar(-l + '0');
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + '0');
+		return (l);
+	}
+}
