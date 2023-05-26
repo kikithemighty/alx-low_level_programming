d7c9257ac48381dc58a4f77550f65a9daead6c70
kikithemighty <shekinahglorymoshepeter02@gmail.com>
2023-05-26 15:56:28 -0700

    10-main.c 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @n1: first number
+ *
+ * @n2: second number
+ *
+ * Return: return results
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
