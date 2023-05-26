b4a2507770cc0ea987aebe9470c264437ef821ab
kikithemighty <shekinahglorymoshepeter02@gmail.com>
2023-05-26 15:10:49 -0700

    1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * description - prints thealphabets in lowercase
+ *
+ * Return: none
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
