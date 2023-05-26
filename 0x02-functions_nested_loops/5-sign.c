9fd4c1872e2df541f39ac5d2a485cef665c39172
kikithemighty <shekinahglorymoshepeter02@gmail.com>
2023-05-26 15:36:48 -0700

    5-main.c 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * @n: a function that prints the sign of a number n
+ *
+ * Return: return 1 if n is posiitive, 0 if n is 0, -1 if n is negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
